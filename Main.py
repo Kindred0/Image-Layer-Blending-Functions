bf09c731b1d0f0e8b465bb09ee5fe7d1a7699ab9
Lenat Thomas <95519891+Kindred0@users.noreply.github.com>
2022-09-05 21:41:33 +0530

    Edited

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
-from Blend_Functions import addition
+import Blend_Functions as BF
 import Layer
+import Color_converter as convert
 
 Base_path = input("Enter the path of the Base Image\t: ")
 Upper_path = input("Enter the path of the Upper Image\t: ")
@@ -7,6 +8,10 @@ Upper_path = input("Enter the path of the Upper Image\t: ")
 Base = Layer.layer(Base_path)
 Upper = Layer.layer(Upper_path)
 
-Resultant = addition(Upper, Base)
+Resultant = BF.addition(Upper, Base)
 print("The Resultant Image")
-Resultant.show()
+Resultant.display()
+
+Resultant = BF.subtraction(Upper, Base)
+print("The Resultant Image")
+Resultant.display()
