df0403de0896e58fad87ecfb98e3ff9c7e3948c3
Lenat Thomas <95519891+Kindred0@users.noreply.github.com>
2022-09-05 21:38:07 +0530

    Edited
    Added Functions to convert RGB channels to HSL channels and back

--- Color_converter.py
+++ Color_converter.py
@@ -1,29 +1,15 @@
 
-def Max(a, b) :
-    if a > b :
-        return a
-    elif b > a :
-        return b
-    else :
-        return a
+import Utilities as util
 
 
-def Min(a, b) :
-    if a < b :
-        return a   
-    elif b < a :
-        return b
-    return a 
-
-
-def to_hsv(rgb) :
+def RGBtoHSV(rgb) :
     red = rgb[0]/255
     green = rgb[1]/255
     blue = rgb[2]/255
     alpha = rgb[3]
 
-    cmax = Max(red, Max(blue, green))
-    cmin = Min(red, Min(blue, green))
+    cmax = util.Max(red, util.Max(blue, green))
+    cmin = util.Min(red, util.Min(blue, green))
 
     """ Value calculation """
 
@@ -52,7 +38,7 @@ def to_hsv(rgb) :
     color = [hue , saturation, value, alpha]
     return color
 
-def to_rgb(hsv) :
+def HSVtoRGB(hsv) :
     hue = hsv[0]
     saturation = hsv[1]
     value = hsv[2]
@@ -68,6 +54,100 @@ def to_rgb(hsv) :
     x = chroma * ( 1 - y)
 
     m = value - chroma
+
+    """ Checking for color range """
+
+    if 0 <= hue and hue < 60 :
+        rgb = [chroma, x, 0]
+    elif 60 <= hue and hue < 120 :
+        rgb = [x, chroma, 0]
+    elif 120 <= hue and hue < 180 :
+        rgb = [0, chroma, x]
+    elif 180 <= hue and hue < 240 :
+        rgb = [0, x, chroma]
+    elif 240 <= hue and hue < 300 :
+        rgb = [x, 0, chroma]
+    elif 300 <= hue and hue < 360 :
+        rgb = [chroma, 0, x]
+    else :
+        rgb = [0, 0, 0]
+
+    """ Assigning colors """
+
+    red = (rgb[0] + m) * 255
+    green = (rgb[1] + m) * 255
+    blue = (rgb[2] + m) * 255 
+
+    """ Converting colors to integer values as RGB only supports integers """
+
+    red = int(red)
+    green = int(green)
+    blue = int(blue)
+
+    color = [red, green, blue, alpha]
+
+    return color
+
+
+def RGBtoHSL(rgb) :
+    red = rgb[0]/255
+    green = rgb[1]/255
+    blue = rgb[2]/255
+    alpha = rgb[3]
+
+    cmax = util.Max(red, util.Max(blue, green))
+    cmin = util.Min(red, util.Min(blue, green))
+
+    delta = cmax - cmin
+
+    """ Luminosity Calculation """
+
+    luminosity = (cmax + cmin) / 2
+
+    """ Saturation Calculation"""
+
+    y = ((2 * luminosity) - 1)
+    if y < 0 :
+        y = y * -1
+
+    if delta == 0 :
+        saturation = 0
+    else : 
+        saturation = delta / (1 - y)
+
+    """ Hue Calculation """
+
+    if delta == 0 :
+        hue = 0
+    elif cmax == red :
+        hue = 60 * (((green - blue) / delta) % 6)
+    elif cmax == green :
+        hue = 60 * (((blue - red) / delta) + 2)
+    elif cmax == blue :
+        hue = 60 * (((red - green) / delta) + 4)
+
+    color = [hue , saturation, luminosity, alpha]
+    return color
+
+def HSLtoRGB(hsl) :
+    hue = hsl[0]
+    saturation = hsl[1]
+    luminosity = hsl[2]
+    alpha = hsl[3]
+
+    """Chroma , X, m calculation"""
+
+    y = ((2 * luminosity) - 1)
+    if y < 0 :
+         y = y * -1
+    chroma = (1 - y) * saturation
+
+    y = (((hue / 60) % 2) - 1)
+    if y < 0 :
+        y = y * -1
+    x = chroma * ( 1 - y)
+
+    m = luminosity - (chroma / 2)
 
     """ Checking for color range """
 
