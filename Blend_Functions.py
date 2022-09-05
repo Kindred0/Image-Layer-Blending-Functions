f2f0e96fe0827ac822ede8e35928c4f33e7658a6
Lenat Thomas <95519891+Kindred0@users.noreply.github.com>
2022-09-05 21:39:12 +0530

    Added New Functions
    Fixed Addition Function and added Subtraction Function

--- Blend_Functions.py
+++ Blend_Functions.py
@@ -1,23 +1,23 @@
+
 import Layer
 
 def addition(Upper, Base) :    
-    dimension = Base.shape
 
     """ The resultant layer takes the dimension of the Base layer and inital matrix """
 
     resultant = Base
 
 
-    """ Finding the smallest of the teo layers """
+    """ Finding the smallest of the two layers """
 
-    if Base.shape[0] < Upper.shape[0] :
-        length = Base.shape[0]
+    if Base.dimension[0] < Upper.dimension[0] :
+        length = Base.dimension[0]
     else :
-        length = Upper.shape[0]
-    if Base.shape[1] < Upper.shape[1] :
-        height = Base.shape[1]
+        length = Upper.dimension[0]
+    if Base.dimension[1] < Upper.dimension[1] :
+        height = Base.dimension[1]
     else :
-        height = Upper.shape[1]
+        height = Upper.dimension[1]
 
     """ The actual addition blending Function """
 
@@ -26,19 +26,128 @@ def addition(Upper, Base) :
 
             """ Inititally the upper layer is projected to the base layer """
 
-            resultant.hsv[i][k] = Upper.hsv[i][k]
+            resultant.rgb[i][k] = Upper.rgb[i][k]
 
-            """ The value points in the HSV matrix is replaced the sum of the value points of the two layers"""
+            """ Checks if the alpha is greater for the Upper layer """
 
-            value = Base.hsv[i][k][2] + Upper.hsv[i][k][2]
-            if value > 100 :
-                value = 100
-            elif value < 0 :
-                value = 0
-            resultant.hsv[i][k][2] = value
+            if Base.rgb[i][k][3] > Upper.rgb[i][k][3] :
 
-    resultant.Update_RGB()
+                """ If yes, the alpha value is replaced by the greater alpha value """
+
+                resultant.rgb[i][k][3] = Base.rgb[i][k][3]
+
+            """ The RGB channels of the resultant layer is replaced with the sum of the RGB channels of the two layers """
+
+            """ Assigning Red Channel """
+
+            color = (Base.rgb[i][k][0] * (Base.rgb[i][k][3] / 255)) + (Upper.rgb[i][k][0] * (Upper.rgb[i][k][3] / 255))
+
+            if color > 255 :
+                color = 255
+            elif color < 0 :
+                color = 0
+            
+            resultant.rgb[i][k][0] = color
+
+            """ Assigning Green Channel """
+
+            color = (Base.rgb[i][k][1] * (Base.rgb[i][k][3] / 255)) + (Upper.rgb[i][k][1] * (Upper.rgb[i][k][3] / 255))
+
+            if color > 255 :
+                color = 255
+            elif color < 0 :
+                color = 0
+            
+            resultant.rgb[i][k][1] = color
+
+            """ Assigning Blue Channel """
+
+            color = (Base.rgb[i][k][2] * (Base.rgb[i][k][3] / 255)) + (Upper.rgb[i][k][2] * (Upper.rgb[i][k][3] / 255))
+
+            if color > 255 :
+                color = 255
+            elif color < 0 :
+                color = 0
+            
+            resultant.rgb[i][k][2] = color
+
+
+    resultant.Update_HSL()
     return resultant
 
     
-    
+def subtraction(Upper, Base) :
+
+    resultant = Base
+
+    if Base.dimension[0] < Upper.dimension[0] :
+        length = Base.dimension[0]
+    else :
+        length = Upper.dimension[0]
+    if Base.dimension[1] < Upper.dimension[1] :
+        height = Base.dimension[1]
+    else :
+        height = Upper.dimension[1]
+
+    """ The actual subtraction blending Function """
+
+    for i in range(0, length) :
+        for k in range(0, height) :
+
+            resultant.rgb[i][k] = Upper.rgb[i][k]
+
+            if Base.rgb[i][k][3] > Upper.rgb[i][k][3] :
+                resultant.rgb[i][k][3] = Base.rgb[i][k][3]
+
+            """ The RGB channels of the resultant layer is replaced with the RGB channels of Upper subtracted with RGB channels of Base """
+
+            """ Assigning Red Channel """
+
+            color = (Base.rgb[i][k][0] * (Base.rgb[i][k][3] / 255)) - (Upper.rgb[i][k][0] * (Upper.rgb[i][k][3] / 255))
+
+            if color > 255 :
+                color = 255
+            elif color < 0 :
+                color = 0
+            
+            resultant.rgb[i][k][0] = color
+
+            """ Assigning Green Channel """
+
+            color = (Base.rgb[i][k][1] * (Base.rgb[i][k][3] / 255)) - (Upper.rgb[i][k][1] * (Upper.rgb[i][k][3] / 255))
+
+            if color > 255 :
+                color = 255
+            elif color < 0 :
+                color = 0
+            
+            resultant.rgb[i][k][1] = color
+
+            """ Assigning Blue Channel """
+
+            color = (Base.rgb[i][k][2] * (Base.rgb[i][k][3] / 255)) - (Upper.rgb[i][k][2] * (Upper.rgb[i][k][3] / 255))
+
+            if color > 255 :
+                color = 255
+            elif color < 0 :
+                color = 0
+            
+            resultant.rgb[i][k][2] = color
+
+
+    resultant.Update_HSL()
+    return resultant
+
+
+"""def normal(Upper, Base) :
+
+    resultant = Base
+
+    if Base.dimension[0] < Upper.dimension[0] :
+        length = Base.dimension[0]
+    else :
+        length = Upper.dimension[0]
+    if Base.dimension[1] < Upper.dimension[1] :
+        height = Base.dimension[1]
+    else :
+        height = Upper.dimension[1]"""
