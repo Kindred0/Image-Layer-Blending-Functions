2ee96544060bce2d0d998a0b7ef7b6a3fdc4b209
Lenat Thomas <95519891+Kindred0@users.noreply.github.com>
2022-09-05 21:42:24 +0530

    Added Utility Functions
    Put the Utility Functions on separate file

---
+++ Utilities.py
@@ -0,0 +1,15 @@
+def Max(a, b) :
+    if a > b :
+        return a
+    elif b > a :
+        return b
+    else :
+        return a
+
+
+def Min(a, b) :
+    if a < b :
+        return a   
+    elif b < a :
+        return b
+    return a 
